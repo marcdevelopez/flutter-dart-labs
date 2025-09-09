5ddc73629650ad9172464006b9a43175954ba491
marcdevelopez <marcdevelopez@gmail.com>
2025-09-09 11:02:55 -0400

    Exercise: start Dart variables and types practice

---
+++ mini_exercises/3_variables_and_types.dart
@@ -0,0 +1,34 @@
+// TODO: Import necessary libraries if input or async is needed (not required for now)
+
+/// Global constant for the store name
+
+void main() {
+  // TODO: Generate product ID (use 'generateProductId' function)
+
+  // TODO: Declare the product name using 'var'
+
+  // TODO: Declare initial stock as 'int'
+
+  // TODO: Declare price as 'double'
+
+  // TODO: Declare availability status as 'bool'
+
+  // TODO: Create a list of product tags (List<String>)
+
+  // TODO: Create a map with supplier data (Map<String, String>)
+
+  // TODO: Declare product image as nullable (String?)
+
+  // -----------------------
+
+  // TODO: Print product data using print and string interpolation
+
+  // TODO: Check if 'productImage' is null before printing its value
+
+  // TODO: Simulate changes in data (new name, stock change, new image)
+
+  // TODO: Print updated data
+}
+
+/// TODO: Create 'generateProductId' function that returns a String
+/// Returns a String with prefix and current timestamp
