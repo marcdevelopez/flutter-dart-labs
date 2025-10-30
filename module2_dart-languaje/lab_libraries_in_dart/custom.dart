093a35386fc0f0bd59108104f51a96270d371413
marcdevelopez <marcdevelopez@gmail.com>
2025-10-30 06:43:16 +0100

    feat(module2-lab-libraries): crate and update files custom.dart & main.dart to practice custom library.

---
+++ module2_dart-languaje/lab_libraries_in_dart/custom.dart
@@ -0,0 +1,15 @@
+// ---------------------------------------------
+// Custom Library (user-defined)
+// ---------------------------------------------
+
+// Here we define a custom library named math_utils
+library math_utils;
+
+// we can create functions that can be reused in other files
+int add(int a, int b) {
+  return a + b;
+}
+
+int subtract(int a, int b) {
+  return a - b;
+}
