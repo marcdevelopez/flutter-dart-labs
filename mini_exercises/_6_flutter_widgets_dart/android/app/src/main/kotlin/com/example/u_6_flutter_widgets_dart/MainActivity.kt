8e21e99983a574e36ccb5ed53d13692f5d65fd28
marcdevelopez <marcdevelopez@gmail.com>
2025-11-25 14:22:26 +0100

    refactor(repo): normalize mini exercise names and paths

--- mini_exercises/_6_flutter_widgets_dart/android/app/src/main/kotlin/com/example/u_6_flutter_widgets_dart/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.u_6_flutter_widgets_dart
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ mini_exercises/flutter_widgets_dart/android/app/src/main/kotlin/com/example/flutter_widgets_dart/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_widgets_dart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
